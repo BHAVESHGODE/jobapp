eb56164b4a444726e455654b5ab66eefe49b6952
BHAVESHGODE <109063646+BHAVESHGODE@users.noreply.github.com>
2025-07-29 15:37:08 +0530

    Update app.js

--- JobJet-backend/app.js
+++ JobJet-backend/app.js
@@ -10,11 +10,7 @@ import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./middlewares/error.js";
 app.use(
-  cors({
-    origin: ["*"],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
+  cors()
 );
 app.use(
   fileUpload({
