6a403044941e0026ed74239dd7b435dd9dde72b3
BHAVESHGODE <109063646+BHAVESHGODE@users.noreply.github.com>
2025-07-29 15:19:59 +0530

    Update app.js

--- JobJet-backend/app.js
+++ JobJet-backend/app.js
@@ -11,7 +11,7 @@ import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./middlewares/error.js";
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://jobjet-frontend.vercel.app"],
+    origin: ["*"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
